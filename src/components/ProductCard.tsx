2e5dd22fc12daf3a968001074090ff92a712203b
MinhKhang0715 <nkhang982@gmail.com>
2022-10-23 13:02:25 +0700

    Added some shadow effects to ProductCard

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 import { productProps } from './models';
 import './style.css'
 
 export default function ProductCard(props: productProps) {
   return (
     <div>
-      <Card sx={{ maxWidth: 212, height: 356, mb: 1 }} key={props.id}>
-        <CardActionArea onClick={() => { window.open('_blank')?.focus() }}>
-          <CardMedia
+      <Card sx={{ maxWidth: 212, mb: 2, boxShadow: '0px 0px 5px 0px rgba(0, 0, 0, 0.5)' }} key={props.id}>
+        <CardActionArea sx={{ height: 356 }} onClick={() => { window.open('_blank')?.focus() }}>
+          <CardMedia  
             sx={{ objectFit: "contain" }}
             component="img"
             height="250"
